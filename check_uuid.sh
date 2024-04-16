feabd4cefe38d8816096d3691d54b508dfc419ee
Jason Wood <jason.wood@purple.telstra.com>
2024-04-16 14:44:46 +1000

    Add first file

---
+++ check_uuid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while IFS= read -r line; do
+  fs=$(echo "$line" | awk '{print $3}')
+#  echo $fs
+
+  if [[ $line != \#* && ($fs == "ext3" || $fs == "ext4" || $fs == "xfs") && ! $line == UUID* ]]; then
+    echo "Error: $line does not start with UUID"
+    exit 1
+  else
+    echo "Pass: $line starts with UUID"
+  fi
+done < /etc/fstabgit push origin
